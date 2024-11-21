f1e3725b30e18518861baa2792d478e76715a5a1
Meghana Vinakota <meghana.penugonda@gmail.com>
2024-11-21 12:04:42 +0000

    Added allowed host

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['boutique-ado-meghana.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['boutique-ado-meghana-7e79711cc1bf.herokuapp.com', 'boutique-ado-meghana.herokuapp.com', 'localhost']
 
 
 # Application definition
